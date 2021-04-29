2392162ad02f90de5351682b93cebacc4eb986fc
Gerson Junior <ger_son_junior@live.com.pt>
2021-04-29 21:14:32 +0100

    Class jogador, falta alterar o defesa, guarda_redes, anavado e medio

--- Jogador.java
+++ Jogador.java
@@ -3,42 +3,19 @@
 
 public class Jogador
 {
-    private static final int Guarda_Redes = 0;
-    private static final int Defesa = 1;
-    private static final int Medio = 2;
-    private static final int Avancado = 3;
-    private static final int Lateral = 4;
-    
-    private String nome;
-    private int posicao;
-    private double valorJogador;
-    private int velocidade;
-    private int resistencia;
-    private int destreza;
-    private int implusao;
-    private int cabecear;
-    private int remate;
-    private int passe;
-    private int elasticidade;
-    
-    //Construtor por omissão
-    public Jogador(){
-        this.nome = "";
-        this.posicao = -1;
-        this.valorJogador = 0;
-        this.velocidade = 0;
-        this.resistencia = 0;
-        this.destreza = 0;
-        this.implusao = 0;
-        this.cabecear = 0;
-        this.remate = 0;
-        this.passe = 0;
-        this.elasticidade = 0;
-    }
-    
+    public String nome;
+    public double valorJogador;
+    public int velocidade;
+    public int resistencia;
+    public int destreza;
+    public int implusao;
+    public int cabecear;
+    public int remate;
+    public int passe;
+        
     //Construtor parametrizado
-    public Jogador(int posicao,double valorJogador,int velocidade,int resistencia,int destreza,int implusao,int cabecear,int remate,int passe,int elasticidade){
-        this.posicao = posicao;
+    public Jogador(String nome, double valorJogador, int velocidade, int resistencia, int destreza, int implusao, int cabecear, int remate, int passe){
+        this.nome = nome;
         this.valorJogador = valorJogador;
         this.velocidade = velocidade;
         this.resistencia = resistencia;
@@ -47,12 +24,11 @@ public class Jogador
         this.cabecear = cabecear;
         this.remate = remate;
         this.passe = passe;
-        this.elasticidade = elasticidade;
     }
     
     //Construtor de copia
     public Jogador(Jogador umJogador){
-        this.posicao = umJogador.getPosicao();
+        this.nome = umJogador.getNome();
         this.valorJogador = umJogador.getValorJogador();
         this.velocidade = umJogador.getVelocidade();
         this.resistencia = umJogador.getResistencia();
@@ -61,11 +37,10 @@ public class Jogador
         this.cabecear = umJogador.getCabecear();
         this.remate = umJogador.getRemate();
         this.passe = umJogador.getPasse();
-        this.elasticidade = umJogador.getElasticidade();
-    }
+    }  
     
-    public int getPosicao(){
-        return this.posicao;
+    public String getNome(){
+        return this.nome;
     }
     
     public double getValorJogador(){
@@ -100,14 +75,10 @@ public class Jogador
         return this.passe;
     }
     
-    public int getElasticidade(){
-        return this.elasticidade;
-    }
-    
     //--------------------------------------------------------------------------\\
     
-    public void setPosicao(int posicao){
-        this.posicao = posicao;
+    public void setValorJogador(String nome){
+        this.nome = nome;
     }
     
     public void setValorJogador(double valorJogador){
@@ -140,30 +111,15 @@ public class Jogador
     
     public void setPasse(int passe){
         this.passe = passe;
-    }
-    
-    public void setElasticidade(int elasticidade){
-        this.elasticidade = elasticidade;
-    }
+    } 
     
     //Metodo para saber o overall do Jogador
-    public double ValorJogador(){
-        if(posicao == Guarda_Redes){
-            this.valorJogador = 0.6*this.elasticidade + 0.1*this.remate + 0.3*this.destreza;
-        }
-        if(posicao == Defesa){
-            this.valorJogador = 0.5*this.destreza+0.3*this.passe + 0.2*(this.velocidade + this.resistencia + this.implusao + this.remate + this.cabecear);
-        }
-        if(posicao == Medio){
-            this.valorJogador = 0.7*(this.passe+this.destreza+this.velocidade) + 0.3*(this.resistencia + this.cabecear + this.remate+this.implusao);
-        }
-        if(posicao == Avancado){
-            this.valorJogador = 0.8*(this.implusao + this.remate + this.cabecear + this.resistencia) + 0.1*this.velocidade + 0.1*(this.destreza+this.passe);
-        }
-        if(posicao == Lateral){
-            this.valorJogador = 0.7*(this.implusao + this.velocidade + this.resistencia) + 0.2*(this.cabecear + this.remate) + 0.1*(this.destreza + this.passe);
-        }
-        return valorJogador;
+    public double valorJogador(int velocidade, int resistencia, int destreza, int implusao, int cabecear, int remate, int passe){
+        return ((double) velocidade + (double) resistencia + (double) destreza + (double) implusao + (double)cabecear + (double) remate + (double) passe) / 7;
     }
-    
+    /*
+    public void cloneJogador(){
+        return new Jogador(this);
+    }
+    */
 }
--- Lateral.java
+++ Lateral.java
@@ -5,57 +5,21 @@
  * @author (Gerson Junior) 
  * @version (número de versão ou data)
  */
+
 public class Lateral extends Jogador
 {
-    private double valorJogador;
-    private int velocidade;
-    private int resistencia;
-    private int destreza;
-    private int implusao;
-    private int cabecear;
-    private int remate;
-    private int passe;
     
     //Construtor por omissão
-    public Lateral(){
-        this.valorJogador = 0;
-        this.velocidade = 0;
-        this.resistencia = 0;
-        this.destreza = 0;
-        this.implusao = 0;
-        this.cabecear = 0;
-        this.remate = 0;
-        this.passe = 0;
+    public Lateral(String nome,int posicao,double valorJogador,int velocidade,int resistencia,int destreza,int implusao,int cabecear,int remate,int passe){
+        super(nome, valorJogador, velocidade, resistencia, destreza, implusao, cabecear, remate, passe);
     }
     
-    //Construtor parametrizado
-    public Lateral(double valorJogador,int velocidade,int resistencia,int destreza,int implusao,int cabecear,int remate,int passe){
-        this.valorJogador = valorJogador;
-        this.velocidade = velocidade;
-        this.resistencia = resistencia;
-        this.destreza = destreza;
-        this.implusao = implusao;
-        this.cabecear = cabecear;
-        this.remate = remate;
-        this.passe = passe;
-    }
-    
-    //Construtor de copia
-    public Lateral(Jogador umLateral){
-        this.valorJogador = umLateral.getValorJogador();
-        this.velocidade = umLateral.getVelocidade();
-        this.resistencia = umLateral.getResistencia();
-        this.destreza = umLateral.getDestreza();
-        this.implusao = umLateral.getImplusao();
-        this.cabecear = umLateral.getCabecear();
-        this.remate = umLateral.getRemate();
-        this.passe = umLateral.getPasse();
-    }
-    
+    public int cruzamento;
+
     //Metodo para saber o overall do Jogador
-    public double valorLateral(){
-        this.valorJogador = 0.7*(this.implusao + this.velocidade + this.resistencia) + 0.2*(this.cabecear + this.remate) + 0.1*(this.destreza + this.passe);
-        return valorJogador;
+    public double valorJogador(int velocidade,int resistencia,int destreza,int implusao,int cabecear,int remate,int passe){
+        return 0.7*((double) velocidade + (double) resistencia + (double) this.cruzamento) + 0.2*((double) cabecear + (double) remate + (double)implusao) + 0.1*((double) destreza + (double) passe);
+    
     }
     
 }
